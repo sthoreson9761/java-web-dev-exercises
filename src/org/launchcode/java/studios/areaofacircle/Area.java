e436eeb0fcac49848adf198cec26dea1f583ee5e
Scott Thoreson <scott.thoreson@live.com>
2022-01-25 16:36:58 -0500

    Added a do-while loop so the user can try another input if the one they just entered was invalid

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -4,10 +4,19 @@ import java.util.Scanner;
 public class Area {
     public static void main (String[] arg){
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a radius: ");
-        Double r = input.nextDouble();
-        Circle circle = new Circle();
-        System.out.println("The area of a circle of radius " + r + " is: " + circle.getArea(r));
+
+        Double r;
+        String initialInput;
+        do{
+            System.out.print("Enter a radius: ");
+            initialInput = input.nextLine();
+            if(Circle.inputValidation(initialInput)){
+                r = Double.parseDouble(initialInput);
+                System.out.println("The area of a circle of radius " + r + " is: " + Circle.getArea(r));
+            }else {
+                System.out.println("Invalid Input");
+            }
+        }while(Circle.inputValidation(initialInput) == false);
 
     }
 }

1fd8b92bb117e7046b051936f730a3cff8613aa5
Scott Thoreson <scott.thoreson@live.com>
2022-01-25 16:38:55 -0500

    Added an input validation method which takes a string and determines if the user input valid information

--- src/org/launchcode/java/studios/areaofacircle/Circle.java
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -4,4 +4,26 @@ public class Circle {
     public static Double getArea(Double radius){
         return 3.14 * radius * radius;
     }
+    public static boolean inputValidation(String str){
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int count = 0;
+        if(str.isEmpty()){
+            return false;
+        }
+        for(int i = 0; i<str.length();i++){
+            char test = str.toLowerCase().charAt(i);
+            if(alphabet.contains(""+test)){
+                return false;
+            }
+            if(test == '.'){
+                count++;
+            }
+        }
+        if(count > 1 || Double.parseDouble(str) < 0){
+            return false;
+        }else{
+            return true;
+        }
+
+    }
 }

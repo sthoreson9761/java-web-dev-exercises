43d10cf9940b7652552bb861fedd869ffa0658ca
Scott Thoreson <scott.thoreson@live.com>
2022-01-31 14:13:59 -0500

    Chapter 3 studio

--- src/org/launchcode/java/studios/countingcharacters/CharacterHash.java
+++ src/org/launchcode/java/studios/countingcharacters/CharacterHash.java
@@ -1,2 +1,24 @@
-package org.launchcode.java.studios.countingcharacters;public class CharacterHash {
+package org.launchcode.java.studios.countingcharacters;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+public class CharacterHash {
+    public static HashMap<Character,Integer> charHashOutput(char[] input){
+        HashMap<Character, Integer> outputHash = new HashMap<>();
+        char[] testArray = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for(char c: input){
+            if(!alphabet.contains(Character.toString(c))){
+
+            }else if(outputHash.size()==0){
+                outputHash.put(c,1);
+            }else if(outputHash.containsKey(c) && Objects.equals(outputHash.get(c),outputHash.get(c))){
+                outputHash.put(c, outputHash.get(c)+1);
+            }else{
+                outputHash.put(c,1);
+            }
+        }
+        return outputHash;
+    }
 }
--- src/org/launchcode/java/studios/countingcharacters/CountCharacters.java
+++ src/org/launchcode/java/studios/countingcharacters/CountCharacters.java
@@ -1,2 +1,37 @@
-package org.launchcode.java.studios.CountingCharacters;public class CountCharacters {
+package org.launchcode.java.studios.countingcharacters;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class CountCharacters {
+    public static void main (String[] arg){
+        Scanner input = new Scanner(System.in);
+        String sample = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        System.out.print("Enter a String to count its characters: ");
+        String userIn = input.nextLine();
+        char[] charactersInString = userIn.toLowerCase().toCharArray();
+
+            HashMap<Character, Integer> outputHash = CharacterHash.charHashOutput(charactersInString);
+
+            for(char h: outputHash.keySet()){
+                System.out.println(h + ": " + outputHash.get(h));
+            }
+
+
+
+
+        input.close();
+    }
+//    public static char[] nonAlpha(String input){
+//        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+//        String strOut="";
+////        char[] inputArry = input.toCharArray();
+//        for(int i = 0;i< input.length();i++){
+//            if(!alphabet.contains(Character.toString(input.charAt(i)))){
+//                strOut = input.substring(0,i)+input.substring(i+1);
+//            }
+//        }
+//        return strOut.toCharArray();
+//    }
 }
